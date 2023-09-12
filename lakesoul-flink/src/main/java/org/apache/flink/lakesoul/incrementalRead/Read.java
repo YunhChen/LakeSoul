cbc50d8b8c68265c5539282bd9474e31e448b60c
ChenYunHey <1908166778@qq.com>
2023-09-12 14:39:10 +0800

    readByStreaming

--- lakesoul-flink/src/main/java/org/apache/flink/lakesoul/incrementalRead/Read.java
+++ lakesoul-flink/src/main/java/org/apache/flink/lakesoul/incrementalRead/Read.java
@@ -1,8 +1,14 @@
 package org.apache.flink.lakesoul.incrementalRead;
 
+import org.apache.flink.api.common.RuntimeExecutionMode;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.lakesoul.metadata.LakeSoulCatalog;
+import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.environment.CheckpointConfig;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.EnvironmentSettings;
 import org.apache.flink.table.api.TableEnvironment;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 import org.apache.flink.table.catalog.Catalog;
 public class Read {
 
@@ -16,12 +22,17 @@ public class Read {
         String startTime = parameters.get(START_TIME,"2023-09-08 12:00:00");
         String endTime = parameters.get(END_TIME,"2023-12-31 12:00:00");
 
-        TableEnvironment tEnv = TableEnvironment.create(EnvironmentSettings.inBatchMode());
+        // 创建流式执行环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(new Configuration());
+        env.setRuntimeMode(RuntimeExecutionMode.STREAMING);
+        env.enableCheckpointing(2000, CheckpointingMode.EXACTLY_ONCE);
+        env.getCheckpointConfig().setExternalizedCheckpointCleanup(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
+        StreamTableEnvironment tEnvs = StreamTableEnvironment.create(env);
+        //设置lakesoul 类型的catalog
         Catalog lakesoulCatalog = new LakeSoulCatalog();
-
-        tEnv.registerCatalog("lakeSoul",lakesoulCatalog);
-        tEnv.useCatalog("lakeSoul");
-        tEnv.useDatabase("default");
+        tEnvs.registerCatalog("lakeSoul", lakesoulCatalog);
+        tEnvs.useCatalog("lakeSoul");
+        tEnvs.useDatabase("default");
 
         String sqlOps = "/*+ OPTIONS('readtype'='incremental','readstarttime'='{startTime}','readendtime'='{endTime}') */";
         String sqlOps_ = sqlOps.replace("{startTime}",startTime).replace("{endTime}",endTime);
@@ -30,29 +41,29 @@ public class Read {
                 tableName +
                  sqlOps_ +
                 " group by col4";
-        tEnv.executeSql(sumSql).print();
+        tEnvs.executeSql(sumSql).print();
 
         String groupBySql = "SELECT col4,max(col5),max(col6),min(col7),count(col16) from " +
                 tableName +
                 sqlOps_ +
                 " group by col4";
-        tEnv.executeSql(groupBySql).print();
+        tEnvs.executeSql(groupBySql).print();
 
         String orderSql = "SELECT * from " +
                 tableName+
                 sqlOps_ +
                 " order by col3,id";
-        tEnv.executeSql(orderSql).print();
+        tEnvs.executeSql(orderSql).print();
 
         String whereSql = "SELECT * from "+
                 tableName +
                 sqlOps_ +
                 " where id<10000000";
-        tEnv.executeSql(whereSql).print();
+        tEnvs.executeSql(whereSql).print();
 
         String distinctSql = "SELECT distinct col3 from " +
                 tableName +
                 sqlOps_;
-        tEnv.executeSql(distinctSql).print();
+        tEnvs.executeSql(distinctSql).print();
     }
 }
