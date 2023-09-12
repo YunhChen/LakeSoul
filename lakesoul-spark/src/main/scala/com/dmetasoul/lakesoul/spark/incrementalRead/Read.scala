a9260e5e36cfedf2f3de6af2ee8336fda9c64aa4
ChenYunHey <1908166778@qq.com>
2023-09-12 14:38:05 +0800

    readByStreaming

--- lakesoul-spark/src/main/scala/com/dmetasoul/lakesoul/spark/incrementalRead/Read.scala
+++ lakesoul-spark/src/main/scala/com/dmetasoul/lakesoul/spark/incrementalRead/Read.scala
@@ -3,55 +3,80 @@ package com.dmetasoul.lakesoul.spark.incrementalRead
 import com.dmetasoul.lakesoul.spark.ParametersTool
 import com.dmetasoul.lakesoul.tables.LakeSoulTable
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.lakesoul.LakeSoulOptions
+import org.apache.spark.sql.streaming.Trigger
 
 object Read {
 
   val TABLE_PATH = "lakesoul.tablePath"
   val START_TIME = "statTime"
   val END_TIME = "endTime"
+  val CHECK_POINT_LOCATION = "checkpointLocation"
+  val OUTPUT_DIR = "output.dir"
   def main(args: Array[String]): Unit = {
 
     val parameter = ParametersTool.fromArgs(args)
 
-    val tablePath = parameter.get(TABLE_PATH,"")
+    val tablePath = parameter.get(TABLE_PATH)
 
     val statTime = parameter.get(START_TIME,"2023-09-06 12:00:00")
 
-    val endTime = parameter.get(END_TIME,"2023-12-31 12:00:00")
+    val outputDir = parameter.get(OUTPUT_DIR)
+
+    val checkpointLocation = parameter.get(CHECK_POINT_LOCATION)
 
     val spark = SparkSession.builder().master("local")
       .config("spark.sql.extensions", "com.dmetasoul.lakesoul.sql.LakeSoulSparkSessionExtensio")
       .getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+    val readStream = spark.readStream.format("lakesoul")
+      .option(LakeSoulOptions.PARTITION_DESC, "")
+      .option(LakeSoulOptions.READ_START_TIME, statTime)
+      .option(LakeSoulOptions.TIME_ZONE, "Asia/Shanghai")
+      .option(LakeSoulOptions.READ_TYPE, "incremental")
+      .load(tablePath)
 
-    val lake1 = LakeSoulTable.forPathIncremental(tablePath, "", statTime, endTime)
-
-    lake1.toDF.createTempView("table")
+    readStream.createTempView("table")
 
     spark.sql(
       """
         |select col4,sum(col5),sum(col6),sum(col7) from table group by col4;
-        |""".stripMargin).show(100)
+        |""".stripMargin)
+      .writeStream
+      .format("lakesoul")
+      .outputMode("append")
+      .format("lakesoul")
+      .option("checkpointLocation", checkpointLocation+"test1")
+      .trigger(Trigger.ProcessingTime(1000))
+      .start(outputDir+"test1")
+      .awaitTermination()
 
     spark.sql(
       """
         |select col4,max(col5),max(col6),min(col7),count(16) from table
         |group by col4;
-        |""".stripMargin).show(100)
+        |""".stripMargin)
+      .writeStream
+      .format("lakesoul")
+      .outputMode("append")
+      .format("lakesoul")
+      .option("checkpointLocation", checkpointLocation+"test2")
+      .trigger(Trigger.ProcessingTime(1000))
+      .start(outputDir + "test2")
+      .awaitTermination()
 
     spark.sql(
       """
         |select * from table order by col3,id;
-        |""".stripMargin).show(100)
+        |""".stripMargin).writeStream
+      .format("lakesoul")
+      .outputMode("append")
+      .format("lakesoul")
+      .option("checkpointLocation", checkpointLocation+"test3")
+      .trigger(Trigger.ProcessingTime(1000))
+      .start(outputDir + "test3")
+      .awaitTermination()
 
-    spark.sql(
-      """
-        |select * from table where id<100000000;
-        |""".stripMargin).show(100)
-
-    spark.sql(
-      """
-        |select distinct col3 from table order by col3;
-        |""".stripMargin).show(100)
 
     spark.close()
 
